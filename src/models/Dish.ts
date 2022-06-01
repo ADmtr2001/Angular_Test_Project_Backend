da759d073759874f2419e664a6d6b5d35845dcd5
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-06-01 16:21:09 +0300

    add Dish model

---
+++ src/models/Dish.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Types } from "mongoose";
+
+export interface IDish {
+  title: string;
+  price: number;
+  category: Types.ObjectId;
+  description: string;
+  image: string;
+  ingredients: {name: string, amount: number}[];
+}
+
+const dishSchema = new Schema<IDish>(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide dish title"],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide dish price']
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'Please provide dish category']
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide dish description']
+    },
+    image: {
+      type: String,
+      required: [true, 'Please provide dish image']
+    },
+    ingredients: {
+      type: [{name: String, amount: Number}],
+      required: [true, 'Please provide dish ingredients']
+    },
+  },
+  { timestamps: true }
+);
+
+export const Category = model<IDish>("Dish", dishSchema);
